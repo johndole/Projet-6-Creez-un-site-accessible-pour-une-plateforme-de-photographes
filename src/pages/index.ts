92520a15a4cb131e17bd666021297330fc2a8fa1
johndole <isildur64500@gmail.com>
2024-02-17 17:46:01 +0100

    fetched and display cards of photographers

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,50 +1,38 @@
 import { photographerTemplate } from "../templates/photographer";
+import { Photographer } from "../models/photographer";
+import { getPhotographers } from "../api";
 import "../styles/main.css";
 
-async function getPhotographers() {
-  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
-  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  let photographers = [
-    {
-      name: "Ma data test",
-      id: 1,
-      city: "Paris",
-      country: "France",
-      tagline: "Ceci est ma data test",
-      price: 400,
-      portrait: "account.png",
-    },
-    {
-      name: "Autre data test",
-      id: 2,
-      city: "Londres",
-      country: "UK",
-      tagline: "Ceci est ma data test 2",
-      price: 500,
-      portrait: "account.png",
-    },
-  ];
-  // et bien retourner le tableau photographers seulement une fois récupéré
-  return {
-    // NB Mentor Kevin: Ici OCR a utilisé le spread operator pour créer un tableau final qui contient 3x le contenu de la variable photographers
-    photographers: [...photographers, ...photographers, ...photographers],
-  };
-}
 
-async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section");
+const photographersSection = document.querySelector(".photographer_section");
+const loaderElement = document.getElementById('loader');
 
+document.addEventListener('DOMContentLoaded',function() {
+
+  if (loaderElement) {
+    // Hide the loader
+    loaderElement.style.display = 'none';
+    console.log('Loader hidden successfully.');
+  } else {
+    console.error('Loader element not found.');
+  }
+ 
+async function displayData(photographers: Photographer[]) {
+  // Afficher les photographes
   photographers.forEach((photographer) => {
     const photographerModel = photographerTemplate(photographer);
     const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
+    photographersSection?.appendChild(userCardDOM);
   });
 }
 
+
 async function init() {
   // Récupère les datas des photographes
-  const { photographers } = await getPhotographers();
+  const photographers = await getPhotographers();
   displayData(photographers);
 }
 
+
 init();
+});
