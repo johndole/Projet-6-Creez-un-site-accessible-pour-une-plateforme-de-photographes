03947c1eb7d1d222dd4e435952be9464fc521088
johndole <isildur64500@gmail.com>
2024-02-17 17:35:07 +0100

    add media type

---
+++ src/models/media.ts
@@ -0,0 +1,14 @@
+export type Media = {
+  id: number;
+  photographerId:number;
+  title: string;
+  image: string;
+  likes: number;
+  tagline: string;
+  date: string;
+  price: number;
+  video: string;
+  src: string;
+  alt: string;
+  liked: boolean;
+};
