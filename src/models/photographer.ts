9a04bc25f19399db437293cd00eb85775c5692fd
johndole <isildur64500@gmail.com>
2024-02-17 17:34:40 +0100

    add type Photographer

---
+++ src/models/photographer.ts
@@ -0,0 +1,10 @@
+export type Photographer = {
+    id: string;
+    name: string;
+    city: string;
+    country: string;
+    tagline: string;
+    price: number;
+    portrait : string;
+    email: string;
+  }
